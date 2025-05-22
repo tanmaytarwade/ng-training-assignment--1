e0686dc691c6a7774afcad79c01fec650a91f8e7
tanmaytarwade <109425362+tanmaytarwade@users.noreply.github.com>
2025-05-23 00:31:06 +0530

    Delete src directory

--- src/main/java/com/todo/todoapp/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.todo.todoapp.repository;
-
-import com.todo.todoapp.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
-
--- src/services/taskService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'http://localhost:8080/api';
-
-export const getTasks = () => axios.get(`${BASE_URL}/tasks`);
-export const createTask = (task) => axios.post(`${BASE_URL}/task`, task);
-export const updateTask = (id, task) => axios.put(`${BASE_URL}/task/${id}`, task);
-export const deleteTask = (id) => axios.delete(`${BASE_URL}/task/${id}`);
